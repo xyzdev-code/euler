f2903d99839801242e4d29d8566d67c8739adf8a
xyzdev-code <xiaoyaozi0412@gmail.com>
2024-05-20 10:09:45 +0000

    added unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
+use colour::red;
 use std::env;
 use std::fs;
+use std::process::exit;
 mod tokenizer;
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,13 +14,22 @@ fn main() {
             cmd_args.push(arg.1.to_string())
         } 
     });
-    println!("Euler Virtual Machine.
-        Executing file {}
-    ", file_path);
+    println!("Euler Virtual Machine. V{}.
+      Executing file {}
+    ", env!("CARGO_PKG_VERSION"),file_path);
     match fs::read_to_string(file_path) {
         Ok(x) => println!("{:?}", tokenizer::tokenize(x)),
-        Err(y) => panic!("Cannot read file, error is {}", {y})
+        Err(y) => {
+            red!("Cannot read file. {} \n.", {y});
+            exit(1)
+        }
     };
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    fn 
 }
 
 
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,7 @@
-use std::{iter::Enumerate, str::Chars, vec};
+use std::{iter::Enumerate, str::Chars, vec, process::exit};
+use colour::red;
 #[derive(Debug)]
+#[derive(PartialEq, Eq)]
 pub enum Tokens{
     // Single Char tokens
     LPAREN,
@@ -42,34 +44,60 @@ pub fn tokenize(program: String)->Vec<Tokens>{
     let line_iter = program.lines().enumerate();
     line_iter.for_each(|line|{
         let char_iter = line.1.chars().enumerate();
-        let mut ptr_char_iter = char_iter.clone();
+        let mut char_iter_copy = char_iter.clone();
+        let mut skip = false;
         char_iter.for_each(|ch|{
-            match ch.1 {
-                '(' => final_tokens.push(Tokens::LPAREN),
-                ')' => final_tokens.push(Tokens::RPAREN),
-                '{' => final_tokens.push(Tokens::LBRACE),
-                '}' => final_tokens.push(Tokens::RBRACE),
-                ',' => final_tokens.push(Tokens::COMMA),
-                '.' => final_tokens.push(Tokens::DOT),
-                '-' => final_tokens.push(Tokens::MINUS),
-                '+' => final_tokens.push(Tokens::PLUS),
-                '*' => final_tokens.push(Tokens::STAR),
-                '/' => final_tokens.push(Tokens::SLASH),
-                ':' => final_tokens.push(Tokens::COLON),
-                '=' => {
-                    if next_char_equal(&mut ptr_char_iter, &ch.0+1, '='){
-                        final_tokens.push(Tokens::EQUALS)
-                    }else{
-                        final_tokens.push(Tokens::EQUAL)
+            if skip == false {
+                match ch.1 {
+                    '(' => final_tokens.push(Tokens::LPAREN),
+                    '{' => final_tokens.push(Tokens::LBRACE),
+                    ')' => final_tokens.push(Tokens::RPAREN),
+                    '}' => final_tokens.push(Tokens::RBRACE),
+                    ',' => final_tokens.push(Tokens::COMMA),
+                    '.' => final_tokens.push(Tokens::DOT),
+                    '-' => final_tokens.push(Tokens::MINUS),
+                    '+' => final_tokens.push(Tokens::PLUS),
+                    '*' => final_tokens.push(Tokens::STAR),
+                    '/' => {
+                        if next_char_equal(&mut char_iter_copy, ch.0 + 1, '/'){
+                            final_tokens.push(Tokens::COMMENT);
+                            skip = true
+                        }else{
+                            final_tokens.push(Tokens::SLASH)
+                        }
+                        final_tokens.push(Tokens::SLASH)
+                    },
+                    ':' => final_tokens.push(Tokens::COLON),
+                    '=' => {
+                        if next_char_equal(&mut char_iter_copy, ch.0+1, '='){
+                            final_tokens.push(Tokens::EQUALS);
+                            skip = true;
+                        }else{
+                            final_tokens.push(Tokens::EQUAL)
+                        }
+                    },
+                    _ => {
+                        red!("Syntax Error: ");
+                        println!("Unexpected token at line {} character {}.
+        {}| {}
+           {}^-- Unexpected '{}' token.", line.0 + 1, ch.0 + 1, line.0 + 1, line.1, " ".repeat(ch.0), ch.1);
+           exit(1)
                     }
-                },
-                _ => panic!(
-                    "Syntax error: Unexpected token at line {} character {}.
-{}|{}
-   {}^-- Unexpected '{}' token.
-                ", line.0 + 1, ch.0 + 1, line.0 + 1, line.1, " ".repeat(ch.0), ch.1)
-            }
-        })
+                    }  
+        }else{
+            skip = false;
+        }});
     });
     return final_tokens
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn correct_tokenization(){
+        assert_eq!(tokenize("==".to_string()), vec!(Tokens::EQUALS));
+        assert_eq!(tokenize("()".to_string()), vec!(Tokens::LPAREN, Tokens::RPAREN))
+    }
 }
