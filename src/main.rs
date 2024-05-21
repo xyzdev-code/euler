4196459a6716f251e4e4598bd1c097666b9a96b2
xyzdev-code <xiaoyaozi0412@gmail.com>
2024-05-21 13:09:46 +0000

    formatted

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,32 @@ use std::process::exit;
 mod tokenizer;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut cmd_args: Vec<String> = vec!();
-    let mut file_path: String = String::new(); 
-    args.iter().enumerate().for_each(|arg|{
-        if arg.0 == args.len() - 1{
+    let mut cmd_args: Vec<String> = vec![];
+    let mut file_path: String = String::new();
+    args.iter().enumerate().for_each(|arg| {
+        if arg.0 == args.len() - 1 {
             file_path = arg.1.to_string()
-        }else{
+        } else {
             cmd_args.push(arg.1.to_string())
-        } 
+        }
     });
-    println!("Euler Virtual Machine. V{}.
+    println!(
+        "Euler Virtual Machine. V{}.
       Executing file {}
-    ", env!("CARGO_PKG_VERSION"),file_path);
+    ",
+        env!("CARGO_PKG_VERSION"),
+        file_path
+    );
     match fs::read_to_string(file_path) {
         Ok(x) => println!("{:?}", tokenizer::tokenize(x)),
         Err(y) => {
-            red!("Cannot read file. {} \n.", {y});
+            red!("Cannot read file. {} \n.", { y });
             exit(1)
         }
     };
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
-    fn 
 }
-
-
-
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,8 +1,7 @@
-use std::{iter::Enumerate, str::Chars, vec, process::exit};
 use colour::red;
-#[derive(Debug)]
-#[derive(PartialEq, Eq)]
-pub enum Tokens{
+use std::{iter::Enumerate, process::exit, str::Chars, vec};
+#[derive(Debug, PartialEq, Eq)]
+pub enum Tokens {
     // Single Char tokens
     LPAREN,
     RPAREN,
@@ -36,17 +35,17 @@ pub enum Tokens{
     ELSEIF,
     ELSE,
 }
-fn next_char_equal(i: &mut Enumerate<Chars>, pos: usize, ch:char)->bool{
-    return i.nth(pos).eq(&Some((pos, ch)))
+fn next_char_equal(i: &mut Enumerate<Chars>, pos: usize, ch: char) -> bool {
+    return i.nth(pos).eq(&Some((pos, ch)));
 }
-pub fn tokenize(program: String)->Vec<Tokens>{
+pub fn tokenize(program: String) -> Vec<Tokens> {
     let mut final_tokens: Vec<Tokens> = vec![];
     let line_iter = program.lines().enumerate();
-    line_iter.for_each(|line|{
+    line_iter.for_each(|line| {
         let char_iter = line.1.chars().enumerate();
         let mut char_iter_copy = char_iter.clone();
         let mut skip = false;
-        char_iter.for_each(|ch|{
+        char_iter.for_each(|ch| {
             if skip == false {
                 match ch.1 {
                     '(' => final_tokens.push(Tokens::LPAREN),
@@ -59,45 +58,57 @@ pub fn tokenize(program: String)->Vec<Tokens>{
                     '+' => final_tokens.push(Tokens::PLUS),
                     '*' => final_tokens.push(Tokens::STAR),
                     '/' => {
-                        if next_char_equal(&mut char_iter_copy, ch.0 + 1, '/'){
+                        if next_char_equal(&mut char_iter_copy, ch.0 + 1, '/') {
                             final_tokens.push(Tokens::COMMENT);
                             skip = true
-                        }else{
+                        } else {
                             final_tokens.push(Tokens::SLASH)
                         }
                         final_tokens.push(Tokens::SLASH)
-                    },
+                    }
                     ':' => final_tokens.push(Tokens::COLON),
                     '=' => {
-                        if next_char_equal(&mut char_iter_copy, ch.0+1, '='){
+                        if next_char_equal(&mut char_iter_copy, ch.0 + 1, '=') {
                             final_tokens.push(Tokens::EQUALS);
                             skip = true;
-                        }else{
+                        } else {
                             final_tokens.push(Tokens::EQUAL)
                         }
-                    },
+                    }
                     _ => {
                         red!("Syntax Error: ");
-                        println!("Unexpected token at line {} character {}.
+                        println!(
+                            "Unexpected token at line {} character {}.
         {}| {}
-           {}^-- Unexpected '{}' token.", line.0 + 1, ch.0 + 1, line.0 + 1, line.1, " ".repeat(ch.0), ch.1);
-           exit(1)
+           {}^-- Unexpected '{}' token.",
+                            line.0 + 1,
+                            ch.0 + 1,
+                            line.0 + 1,
+                            line.1,
+                            " ".repeat(ch.0),
+                            ch.1
+                        );
+                        exit(1)
                     }
-                    }  
-        }else{
-            skip = false;
-        }});
+                }
+            } else {
+                skip = false;
+            }
+        });
     });
-    return final_tokens
+    return final_tokens;
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn correct_tokenization(){
+    fn correct_tokenization() {
         assert_eq!(tokenize("==".to_string()), vec!(Tokens::EQUALS));
-        assert_eq!(tokenize("()".to_string()), vec!(Tokens::LPAREN, Tokens::RPAREN))
+        assert_eq!(
+            tokenize("()".to_string()),
+            vec!(Tokens::LPAREN, Tokens::RPAREN)
+        )
     }
 }
